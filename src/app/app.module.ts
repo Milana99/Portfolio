0c0f5c652663627aee0f4f550fc9750b856c6cd1
Milana Radovanovic <milana.radovanovic1@gmail.com>
2024-04-19 12:57:34 +0200

    third page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SecoundPageComponent } from './core/secound-page/secound-page.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ThirdPageComponent } from './core/third-page/third-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstPageComponent,
     SecoundPageComponent,
-    NavBarComponent
+    NavBarComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule,
