e9828d2896353d1d4d4fa4679bd9bee13ef0e966
Milana Radovanovic <milana.radovanovic1@gmail.com>
2024-05-07 15:48:36 +0200

    Person Data for fp added

---
+++ src/app/services/personService/person.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { PERSON } from 'src/app/data/personData';
+import { Person } from 'src/app/model/person';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  private person: Person = new Person;
+
+  constructor() {
+    this.person.id = Number(PERSON.id);
+    this.person.name = PERSON.name.toUpperCase();
+    this.person.occupation = PERSON.occupation;
+    this.person.description = PERSON.description;
+  }
+
+  readPerson(): Person {
+    return this.person;
+  }
+}
